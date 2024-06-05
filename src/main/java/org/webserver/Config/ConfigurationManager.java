7b78cb51a721c87caeecbda5249143ca6b1b194a
Ahanaf Islam <ahanafislamworks@gmail.com>
2024-06-05 22:48:57 +0600

    Conf mnger fixed

--- src/main/java/org/webserver/Config/ConfigurationManager.java
+++ src/main/java/org/webserver/Config/ConfigurationManager.java
@@ -1,5 +1,13 @@
 package org.webserver.Config;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.webserver.util.Json;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
 public class ConfigurationManager {
     public static ConfigurationManager newConfigurationManager;
     private static Configuration newConfiguration;
@@ -13,11 +21,44 @@ public class ConfigurationManager {
     /*
     * To load a configuration file by the path provided
     */
-    public void loadConfigurationFiles(String path){}
+    public void loadConfigurationFiles(String path){
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(path);
+        }
+        catch (FileNotFoundException e){
+            throw new HttpConfigException(e);
+        }
+        StringBuffer sb = new StringBuffer();
+        int i;
+        try {
+            while ((i = fileReader.read()) != -1) {
+                sb.append((char) i);
+            }
+        }
+        catch (IOException e){
+            throw new HttpConfigException(e);
+        }
+        JsonNode conf =null;
+        try {
+            conf = Json.parse(sb.toString());
+        }
+        catch (IOException e){
+            throw new HttpConfigException(e);
+        }
+        }
 
     /*
-    * To save a configuration file by the path provided
-    */
+     * To get the configuration instance
+     */
+    public Configuration getConfiguration() {
+        if (newConfigurationManager == null) {
+            throw new HttpConfigException("Error: ConfigurationManager");
+        }
+        return newConfiguration;
+    }
 
-    public void getConfigurationFiles(){}
 }
+
+
+
