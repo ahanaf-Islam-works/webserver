1cda5e508bedb342de9d0f15c68a0cf19723d761
Ahanaf Islam <ahanafislamworks@gmail.com>
2024-06-06 01:03:14 +0600

    server started

--- src/main/java/org/webserver/Config/ConfigurationManager.java
+++ src/main/java/org/webserver/Config/ConfigurationManager.java
@@ -3,62 +3,49 @@ package org.webserver.Config;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.webserver.util.Json;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileNotFoundException;
 
 public class ConfigurationManager {
-    public static ConfigurationManager newConfigurationManager;
+    private static ConfigurationManager newConfigurationManager;
     private static Configuration newConfiguration;
+
     private ConfigurationManager() {}
-    public ConfigurationManager getInstance() {
-        if (newConfigurationManager == null){
+
+    public static ConfigurationManager getInstance() {
+        if (newConfigurationManager == null) {
             newConfigurationManager = new ConfigurationManager();
         }
         return newConfigurationManager;
     }
+
     /*
-    * To load a configuration file by the path provided
-    */
-    public void loadConfigurationFiles(String path){
-        FileReader fileReader = null;
-        try {
-            fileReader = new FileReader(path);
-        }
-        catch (FileNotFoundException e){
-            throw new HttpConfigException(e);
-        }
-        StringBuffer sb = new StringBuffer();
-        int i;
-        try {
+     * To load a configuration file by the path provided
+     */
+    public void loadConfigurationFiles(String path) {
+        try (FileReader fileReader = new FileReader(path)) {
+            StringBuffer sb = new StringBuffer();
+            int i;
             while ((i = fileReader.read()) != -1) {
                 sb.append((char) i);
             }
-        }
-        catch (IOException e){
+            JsonNode conf = Json.parse(sb.toString());
+            newConfiguration = Json.fromJson(conf, Configuration.class);
+        } catch (FileNotFoundException e) {
+            throw new HttpConfigException(e);
+        } catch (IOException e) {
             throw new HttpConfigException(e);
         }
-        JsonNode conf =null;
-        try {
-            conf = Json.parse(sb.toString());
-        }
-        catch (IOException e){
-            throw new HttpConfigException(e);
-        }
-        }
+    }
 
     /*
      * To get the configuration instance
      */
     public Configuration getConfiguration() {
-        if (newConfigurationManager == null) {
-            throw new HttpConfigException("Error: ConfigurationManager");
+        if (newConfiguration == null) {
+            throw new HttpConfigException("Configuration has not been loaded.");
         }
         return newConfiguration;
     }
-
 }
-
-
-
--- src/main/java/org/webserver/MainServer.java
+++ src/main/java/org/webserver/MainServer.java
@@ -1,8 +1,16 @@
 package org.webserver;
 
+import org.webserver.Config.Configuration;
+import org.webserver.Config.ConfigurationManager;
+
 public class MainServer {
     public static void main(String[] args) {
 
         System.out.println("Starting server ...");
+        ConfigurationManager.getInstance().loadConfigurationFiles("src/main/resources/http.json");
+        Configuration conf = ConfigurationManager.getInstance().getConfiguration();
+
+        System.out.println(conf.getPort());
+        System.out.println(conf.getWebroot());
     }
 }
